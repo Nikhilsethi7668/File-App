e89ac304b2dedf36f18824a4378d2c4c7b6bc622
Adarsh <itsadarsh33@gmail.com>
2025-09-04 21:44:41 +0530

    Update

--- frontend/src/Api/Axios.jsx
+++ frontend/src/Api/Axios.jsx
@@ -2,8 +2,8 @@ import axios from 'axios'
 import React from 'react'
 
 const Axios = axios.create({
-    baseURL: "https://file-app-api.amiigo.in/api",
-    // baseURL: "http://localhost:8493/api",
+    // baseURL: "https://file-app-api.amiigo.in/api",
+    baseURL: "http://localhost:8493/api",
     withCredentials: true,
     timeout: 5000,
     headers: {
